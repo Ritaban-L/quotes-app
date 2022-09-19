4fa7f86281395d8f2065b1a06816d9d75f9c7a50
Ritaban Bandyopadhyay <ritabanbandyopadhyay@Ritabans-MacBook-Pro-2.local>
2022-09-19 08:51:31 +0530

    [Ritaban] Add. Codebase

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const Quote = require("inspirational-quotes");
+
+const app = express();
+
+app.use(
+    function(req,res,next){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
+        next();
+    }
+);
+
+app.get("/",(req,res)=>{
+    res.send(Quote.getQuote());
+});
+
+const port = 8080;
+
+app.listen(port,()=>{
+    console.log("Server started successfully");
+});
